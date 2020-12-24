10da0377ddbcac519ccb4d458cc5616debf81253
Ivan_Taturevich <ivan_taturevich@epam.com>
2020-12-24 18:53:53 +0300

    added Prism MVVM for WPF sample

---
+++ samples/src/WpfApplicationPrism/WpfApplicationPrism/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WpfApplicationPrism
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DispatcherUnhandledException += OnDispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            new Bootstrapper().Run();
+        }
+
+        private static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+            e.Handled = true;
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(((Exception) e.ExceptionObject).Message);
+        }
+    }
+}
---
+++ samples/src/WpfApplicationPrism/WpfApplicationPrism/ViewModels/SecondViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Prism.Commands;
+using WpfApplicationPrism.Services;
+
+namespace WpfApplicationPrism.ViewModels
+{
+    public class SecondViewModel : ViewModelBase
+    {
+        private readonly IPetService _petService;
+        private List<DogImage> _randomDogsDogImages;
+
+        public SecondViewModel(IPetService petService)
+        {
+            _petService = petService;
+            LoadingCommand = new DelegateCommand(async () => await DownloadData());
+        }
+
+        public DelegateCommand LoadingCommand { get; private set; }
+
+        public List<DogImage> RandomDogs
+        {
+            get => _randomDogsDogImages;
+            set => SetProperty(ref _randomDogsDogImages, value);
+        }
+        
+        private async Task DownloadData()
+        {
+            RandomDogs = await _petService.DownloadRandomDogs();
+        }
+
+        public override async Task LoadData()
+        {
+            await base.LoadData();
+            await DownloadData();
+        }
+    }
+}
